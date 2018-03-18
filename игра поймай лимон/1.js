a96f038c8c4d237b5a3557bb94058fae4098678f
Dmitry Kirilenko <36708732+DmitryKirilenko@users.noreply.github.com>
2018-03-18 16:46:58 +0200

    Add files via upload

---
+++ игра поймай лимон/1.js
@@ -0,0 +1,40 @@
+
+var ball=document.getElementById("ball");
+var basket=document.getElementById("basket");
+var score=document.getElementById("out");
+var num = 0;
+var count = 1;
+
+var act=0;
+ball.style.left = Math.floor(Math.random() * 500) + 'px';
+
+function move(){
+ball.style.marginTop= act + "px";
+act=act+ 4;
+if(act == 500){
+if(ball.offsetLeft >= basket.offsetLeft && ball.offsetLeft <= basket.offsetLeft + basket.clientWidth){
+num=num+1
+score.innerHTML = num ; 
+}
+else{		 
+if(count>0){
+alert("вы прогирали , начать заново"); 
+num=0;
+score.innerHTML = 0 ;
+}
+}	
+act = 0;
+ball.style.left = Math.floor(Math.random() * 1500) + 'px';
+}
+	
+}
+var timer=setInterval(move, 10);
+
+document.onmousemove = function(event){
+	 basket.style.left = event.clientX + 'px';
+}
+
+
+
+
+
